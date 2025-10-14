5464303b437c282e33f5cecc6d3adeb63ea362be
leveau10 <leovir957@gmail.com>
2025-10-14 20:28:17 -0300

    feat: banco postgres

--- recipeapp/settings.py
+++ recipeapp/settings.py
@@ -74,12 +74,15 @@ WSGI_APPLICATION = 'recipeapp.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "receitas_db",
+        "USER": "receita_user",
+        "PASSWORD": "receita_password",
+        "HOST": "db",
+        "PORT": 5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
